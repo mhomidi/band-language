6fabffcf372b0ff88f7663563b002a5b7ed39d6b
Hadi Omidi <homidi@uwaterloo.ca>
2022-04-14 18:36:36 -0400

    Add headers for files (#2)

---
+++ Lexer.h
@@ -0,0 +1,12 @@
+enum Token
+{
+    tok_eof = -1,
+    tok_def = -2,
+    tok_identifier = -3,
+    tok_number = -4
+};
+
+static string identifierStr;
+static double numVal;
+
+static int getToken();
